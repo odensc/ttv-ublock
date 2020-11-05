56c42cf3c404265d72d41e39cce106b3f229ba0c
Oden <hello@odensc.com>
2020-11-05 15:40:59 -0800

    Update twitch-videoad.js

--- twitch-videoad.js
+++ twitch-videoad.js
@@ -3,7 +3,7 @@ const origFetch = window.fetch;
 window.fetch = (url, init, ...args) => {
 	if (typeof url === "string") {
 		if (url.includes("/access_token")) {
-			url = url.replace("player_type=site", "player_type=animated_thumbnails");
+			url = url.replace("player_type=site", "player_type=site");
 		} else if (
 			url.includes("/gql") &&
 			init &&
@@ -11,7 +11,7 @@ window.fetch = (url, init, ...args) => {
 			init.body.includes("PlaybackAccessToken")
 		) {
 			const newBody = JSON.parse(init.body);
-			newBody.variables.playerType = "animated_thumbnails";
+			newBody.variables.playerType = "site";
 			init.body = JSON.stringify(newBody);
 		}
 	}
