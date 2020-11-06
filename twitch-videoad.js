765499d7484421c0d96f2372693629a177ffb8c7
Oden <hello@odensc.com>
2020-11-06 09:40:11 -0800

    Disable for now

--- twitch-videoad.js
+++ twitch-videoad.js
@@ -3,16 +3,16 @@ const origFetch = window.fetch;
 window.fetch = (url, init, ...args) => {
 	if (typeof url === "string") {
 		if (url.includes("/access_token")) {
-			url = url.replace("player_type=site", "player_type=site");
+			// url = url.replace("player_type=site", "player_type=site");
 		} else if (
 			url.includes("/gql") &&
 			init &&
 			typeof init.body === "string" &&
 			init.body.includes("PlaybackAccessToken")
 		) {
-			const newBody = JSON.parse(init.body);
-			newBody.variables.playerType = "site";
-			init.body = JSON.stringify(newBody);
+			// const newBody = JSON.parse(init.body);
+			// newBody.variables.playerType = "site";
+			// init.body = JSON.stringify(newBody);
 		}
 	}
 	return origFetch(url, init, ...args);
